470b6e8b5668395542cab893ea2870dfa3222b73
npldevfr <asnpldev@gmail.com>
2024-07-15 11:45:16 +0200

    style: Remove unused useState and index.css file

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import { Button } from '@koops/csui'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Button></Button>
