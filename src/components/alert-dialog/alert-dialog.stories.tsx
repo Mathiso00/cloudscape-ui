0badfb523578df00d14412b0c895c3cc91a6a006
npldevfr <asnpldev@gmail.com>
2024-07-15 17:32:19 +0200

    refactor: Update Default story to use args instead of render function

--- src/components/alert-dialog/alert-dialog.stories.tsx
+++ src/components/alert-dialog/alert-dialog.stories.tsx
@@ -14,32 +14,35 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  render: () => (
-    <>
-      <AlertDialog>
-        <AlertDialog.Trigger>
-          <Button
-            variant="destructive"
-            leftIcon="i-mdi-trash-can"
-          >
-            Delete my account
-          </Button>
-        </AlertDialog.Trigger>
-        <AlertDialog.Content>
-          <AlertDialog.Header>
-            <AlertDialog.Title>
-              Delete your account ?
-            </AlertDialog.Title>
-            <AlertDialog.Description>
-              Are you sure you want to delete your account? This action is irreversible.
-            </AlertDialog.Description>
-          </AlertDialog.Header>
-          <AlertDialog.Footer>
-            <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
-            <AlertDialog.Action>Yes, delete it</AlertDialog.Action>
-          </AlertDialog.Footer>
-        </AlertDialog.Content>
-      </AlertDialog>
-    </>
+  args: {
+    defaultOpen: false,
+    onOpenChange: () => {},
+    open: false,
+  },
+  render: e => (
+    <AlertDialog {...e}>
+      <AlertDialog.Trigger>
+        <Button
+          variant="destructive"
+          leftIcon="i-mdi-trash-can"
+        >
+          Delete my account
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Header>
+          <AlertDialog.Title>
+            Delete your account ?
+          </AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete your account? This action is irreversible.
+          </AlertDialog.Description>
+        </AlertDialog.Header>
+        <AlertDialog.Footer>
+          <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+          <AlertDialog.Action>Yes, delete it</AlertDialog.Action>
+        </AlertDialog.Footer>
+      </AlertDialog.Content>
+    </AlertDialog>
   ),
 }
