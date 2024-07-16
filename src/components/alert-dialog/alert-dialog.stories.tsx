eb027903c192388f01d593fca8078c55809f0bbd
npldevfr <asnpldev@gmail.com>
2024-07-16 16:25:08 +0200

    feat: Add @storybook/addon-actions to package.json

--- src/components/alert-dialog/alert-dialog.stories.tsx
+++ src/components/alert-dialog/alert-dialog.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 import { AlertDialog, Button } from '../index.ts'
 
 const meta = {
@@ -14,10 +15,39 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
+  render: e => (
+    <AlertDialog {...e}>
+      <AlertDialog.Trigger>
+        <Button
+          variant="destructive"
+          leftIcon="i-mdi-trash-can"
+        >
+          Delete my account
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Header>
+          <AlertDialog.Title>
+            Delete your account ?
+          </AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete your account? This action is irreversible.
+          </AlertDialog.Description>
+        </AlertDialog.Header>
+        <AlertDialog.Footer>
+          <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+          <AlertDialog.Action>Yes, delete it</AlertDialog.Action>
+        </AlertDialog.Footer>
+      </AlertDialog.Content>
+    </AlertDialog>
+  ),
+}
+
+export const WithProps: Story = {
   args: {
-    defaultOpen: false,
-    onOpenChange: () => {},
-    open: false,
+    defaultOpen: true,
+    onOpenChange: () => action('on-open-change'),
+    open: true,
   },
   render: e => (
     <AlertDialog {...e}>
