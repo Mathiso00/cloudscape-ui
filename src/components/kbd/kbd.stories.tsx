cab648b97b70c48a27685ce286330fc5d317508d
npldevfr <asnpldev@gmail.com>
2024-07-23 13:38:37 +0200

    feat: Add Kbd component and stories

---
+++ src/components/kbd/kbd.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Kbd } from '../index.ts'
+
+const meta = {
+  title: 'Primitives/Kbd',
+  component: Kbd,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Kbd>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    children: '⌘ + S',
+  },
+}
