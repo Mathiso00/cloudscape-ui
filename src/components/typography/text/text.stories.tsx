daa605e8d826d0b6d28a005249d91c3ef4d02c97
npldevfr <asnpldev@gmail.com>
2024-07-29 17:21:05 +0200

    feat: Add Text component for rendering text with styles

---
+++ src/components/typography/text/text.stories.tsx
@@ -0,0 +1,64 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import type { Size, Weight } from './text.tsx'
+import { Text } from './text.tsx'
+
+const meta = {
+  title: 'Typography/Text',
+  component: Text,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Text>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    weight: 'regular',
+    children: 'The quick brown fox jumps over the lazy dog.',
+  },
+  render: (e) => {
+    const sizes: Size[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    return (
+      <div className="flex flex-col gap-2">
+        {sizes.map(size => (
+          <Text key={size} size={size} {...e} />
+        ))}
+      </div>
+    )
+  },
+}
+
+export const Weights: Story = {
+  args: {
+    size: 6,
+    children: 'The quick brown fox jumps over the lazy dog.',
+  },
+  render: (e) => {
+    const weights: Weight[] = ['light', 'regular', 'medium', 'bold']
+
+    return (
+      <div className="flex flex-col gap-2">
+        {weights.map(weight => (
+          <Text key={weight} weight={weight} {...e} />
+        ))}
+      </div>
+    )
+  },
+}
+
+export const Truncate: Story = {
+  args: {
+    size: 6,
+    weight: 'regular',
+    children: 'The quick brown fox jumps over the lazy dog.',
+  },
+  render: e => (
+    <div className="max-w-[200px] border">
+      <Text truncate {...e} />
+    </div>
+  ),
+}
