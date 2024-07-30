ee04b611b5e424c506a2752c48c8601e603c91eb
npldevfr <asnpldev@gmail.com>
2024-07-30 10:55:16 +0200

    style: Remove commented out code in text component

--- src/components/typography/text/text.tsx
+++ src/components/typography/text/text.tsx
@@ -1,11 +1,9 @@
-import { marginProps } from '@/props/margin.props.ts'
-
-const as = ['span', 'div', 'label', 'p'] as const
-const sizes = ['1', '2', '3', '4', '5', '6', '7', '8', '9'] as const
-
-const textPropsDefinition = {
-  ...marginProps,
-}
+// const as = ['span', 'div', 'label', 'p'] as const
+// const sizes = ['1', '2', '3', '4', '5', '6', '7', '8', '9'] as const
+//
+// const textPropsDefinition = {
+//   ...marginProps,
+// }
 
 function Text() {
 
