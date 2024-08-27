a46124d8375249ec1af57940a1bd488cfd24fe6a
npldevfr <asnpldev@gmail.com>
2024-08-27 12:13:13 +0200

    feat: Add avatar component to primitives folder

--- src/components/primitives/index.ts
+++ src/components/primitives/index.ts
@@ -1,5 +1,9 @@
 export * from './button/index'
+export * from './button-group/index'
 export * from './alert-dialog/index'
 export * from './tooltip/index'
 export * from './dropdown/index'
 export * from './skeleton/index'
+export * from './avatar/index'
+
+
