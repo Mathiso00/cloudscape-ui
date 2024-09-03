88801f015be2d01e1123c0d1221ce25ec12bb75d
npldevfr <asnpldev@gmail.com>
2024-09-03 13:50:21 +0200

    style: Update button styles for different variants and sizes

--- src/components/primitives/button/button.tsx
+++ src/components/primitives/button/button.tsx
@@ -5,24 +5,23 @@ import { tv } from 'tailwind-variants'
 import { cn } from '@/utils'
 
 const buttonVariants = tv({
-  base: 'inline-flex select-none disabled:cursor-not-allowed items-center justify-center whitespace-nowrap text-sm font-medium  transition-all focus-visible:outline-none disabled:opacity-50',
+  base: 'inline-flex text-center shadow select-none gap-2.5 border border-transparent disabled:cursor-not-allowed items-center outline-transparent justify-center whitespace-nowrap font-medium transition-all ring-offset-2 ring-offset-inherit ring-transparent focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50',
   variants: {
     variant: {
-      default: 'bg-white text-neutral-800 hover:bg-white/90',
-      destructive:
-                'bg-red-700 text-red-50 hover:bg-red-700/90 ',
-      outline:
-                'border border-white text-white hover:bg-white bg-transparent hover:text-neutral-900',
-      secondary:
-                'bg-neutral-800 text-white hover:bg-neutral-700',
-      ghost: 'hover:bg-neutral-800/10 bg-transparent hover:text-white/60',
-      link: 'text-white bg-transparent underline-offset-4 hover:underline',
+      default: 'bg-white !text-secondary focus-visible:ring-white/10 focus-visible:ring-offset-transparent',
+      outline: 'border-white/10 text-white bg-transparent hover:bg-white/5 focus-visible:ring-white/5 focus-visible:ring-offset-transparent',
+      destructive: 'bg-red-800 text-white hover:bg-red-600 focus-visible:ring-red-600/20 focus-visible:ring-offset-transparent',
+      secondary: 'bg-secondary text-white hover:bg-secondary/90 focus-visible:ring-secondary/20 focus-visible:ring-offset-transparent',
+      ghost: 'shadow-none hover:bg-neutral-800/10 bg-transparent hover:text-white/60 focus-visible:ring-transparent focus-visible:ring-offset-transparent',
+      link: 'shadow-none text-white bg-transparent underline-offset-4 hover:underline focus-visible:ring-transparent focus-visible:ring-offset-transparent',
+    },
+    block: {
+      true: 'w-full',
     },
     size: {
-      default: 'h-10 px-4 rounded-xl py-2',
-      sm: 'h-9 rounded-xl px-3',
-      lg: 'h-11 rounded-xl px-8',
-      icon: 'h-10 w-10 rounded-xl',
+      default: 'h-37px px-3 rounded-lg text-1rem',
+      lg: 'h-45px px-4 rounded-lg text-1.25rem',
+      icon: 'h-37px w-37px rounded-lg',
     },
   },
   defaultVariants: {
@@ -37,6 +36,7 @@ export interface ButtonProps
   asChild?: boolean
   leftIcon?: string
   isLoading?: boolean
+  block?: boolean
   icon?: string
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
@@ -80,11 +80,11 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {
           isLoading
             ? (
-                <div className="i-svg-spinners:ring-resize mr-2" />
+                <div className="i-svg-spinners:ring-resize" />
               )
             : (
                 leftIcon && (
-                  <div className={`mr-2 ${leftIcon}`} />
+                  <div className={`${leftIcon}`} />
                 )
               )
         }
