e60515a94eee7bd7562a2f8c7653dee4fe4f507f
npldevfr <asnpldev@gmail.com>
2024-09-03 14:02:07 +0200

    style: Update progress bar border style in stories and component

--- src/components/primitives/progressbar/pogressbar.stories.tsx
+++ src/components/primitives/progressbar/pogressbar.stories.tsx
@@ -4,7 +4,6 @@ import { Progress } from './progressbar'
 const meta = {
   title: 'Primitives/Progress',
   component: Progress,
-
   tags: ['autodocs'],
 } satisfies Meta<typeof Progress>
 
@@ -14,6 +13,10 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   args: {
     value: 10,
-
   },
+  render: e => (
+    <div className="w-100">
+      <Progress {...e} />
+    </div>
+  ),
 }
