f9f72d11abfdc8e3fd6b6569ed6c415a4795c37a
npldevfr <asnpldev@gmail.com>
2024-09-12 15:35:37 +0200

    style: Update import paths and component usage in App component

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,24 +1,14 @@
 import {
-  AlertDialog,
+  Button,
 } from '@koopsoperator/csui'
 
 function App() {
   return (
-    <AlertDialog>
-      <AlertDialog.Trigger>Open</AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Header>
-          <AlertDialog.Title>Supprimer votre compte ?</AlertDialog.Title>
-          <AlertDialog.Description>
-            Cette action est irréversible, êtes-vous sûr de vouloir supprimer votre compte ?
-          </AlertDialog.Description>
-        </AlertDialog.Header>
-        <AlertDialog.Footer>
-          <AlertDialog.Cancel>Annuler</AlertDialog.Cancel>
-          <AlertDialog.Action>Supprimer</AlertDialog.Action>
-        </AlertDialog.Footer>
-      </AlertDialog.Content>
-    </AlertDialog>
+    <Button
+      variant="outline"
+    >
+      Hello world
+    </Button>
 
   )
 }
