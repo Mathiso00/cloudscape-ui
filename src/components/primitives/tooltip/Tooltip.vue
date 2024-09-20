ab3af2d61d84b4b792b1ce550f65cde4cc807436
npldevfr <asnpldev@gmail.com>
2024-09-20 11:13:06 +0200

    feat: Add tooltip component and related files

---
+++ src/components/primitives/tooltip/Tooltip.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import TooltipContent from '@/components/primitives/tooltip/TooltipContent.vue'
+import TooltipProvider from '@/components/primitives/tooltip/TooltipProvider.vue'
+import TooltipRoot from '@/components/primitives/tooltip/TooltipRoot.vue'
+import TooltipTrigger from '@/components/primitives/tooltip/TooltipTrigger.vue'
+
+const props = defineProps<{
+  content?: string
+}>()
+</script>
+
+<template>
+  <TooltipProvider>
+    <TooltipRoot>
+      <TooltipTrigger v-bind="props">
+        <slot />
+      </TooltipTrigger>
+      <TooltipContent>
+        <template v-if="content && !$slots.content">
+          {{ content }}
+        </template>
+        <template v-else>
+          <slot name="content" />
+        </template>
+      </TooltipContent>
+    </TooltipRoot>
+  </TooltipProvider>
+</template>
