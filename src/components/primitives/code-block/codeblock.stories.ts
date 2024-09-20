c018c57721fcbce96216bb8f3289e5bc2f8e59a9
npldev <asnpldev@gmail.com>
2024-09-21 01:34:11 +0200

    feat: add copy functionality to CodeBlock.vue

--- src/components/primitives/code-block/codeblock.stories.ts
+++ src/components/primitives/code-block/codeblock.stories.ts
@@ -14,6 +14,22 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
-    code: 'const foo = "bar"',
+    code: `package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}`,
+    lang: 'go',
   },
 }
