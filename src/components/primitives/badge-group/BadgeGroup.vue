977aaee342e16ad3d93175811356f085634cbcbc
npldevfr <asnpldev@gmail.com>
2024-09-25 00:18:22 +0200

    feat: Add new vite plugin for injecting CSS via JS

--- src/components/primitives/badge-group/BadgeGroup.vue
+++ src/components/primitives/badge-group/BadgeGroup.vue
@@ -1,3 +1,6 @@
+<script setup lang="ts">
+</script>
+
 <template>
   <div class="w-fit flex flex-row ![&>*]:rounded-none ![&>*:first-child]:rounded-l-md ![&>*:last-child]:rounded-r-md ![&>*:not(:last-child)]:border-r-none">
     <slot />
--- vite.config.ts
+++ vite.config.ts
@@ -1,63 +1,46 @@
-import { resolve } from 'node:path'
 import * as path from 'node:path'
 import Vue from '@vitejs/plugin-vue'
-import typescript2 from 'rollup-plugin-typescript2'
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
-import Dts from 'vite-plugin-dts'
-import { libInjectCss } from 'vite-plugin-lib-inject-css'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [
-    UnoCSS(),
-    Dts({
-      cleanVueFileName: true,
-      insertTypesEntry: true,
-    }),
-    libInjectCss(),
-    typescript2({
-      check: false,
-      include: ['src/components/**/*.vue'],
-      tsconfigOverride: {
-        compilerOptions: {
-          outDir: 'dist',
-          sourceMap: true,
-          declaration: true,
-          declarationMap: true,
-        },
-      },
-      exclude: ['vite.config.ts'],
-    }),
     Vue(),
+    UnoCSS(),
+    dts({
+      tsconfigPath: 'tsconfig.build.json',
+      cleanVueFileName: true,
+      exclude: ['src/test/**', 'src/**/*.story.ts'],
+    }),
+    cssInjectedByJsPlugin(),
   ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   build: {
-    cssCodeSplit: true,
     lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: 'src/index.ts',
       name: '@koopsoperator/csui',
-      formats: ['es', 'cjs', 'umd'],
-      fileName: format => `koops-csui.${format}.js`,
+      fileName: (format, name) => {
+        return `${name}.${format === 'es' ? 'js' : 'umd.cjs'}`
+      },
+      entry: {
+        index: path.resolve(__dirname, 'src/index.ts'),
+      },
     },
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'src/index.ts'),
-      },
       external: ['vue'],
       output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'main.css')
-            return 'my-library-vue-ts.css'
-          return assetInfo.name
-        },
-        exports: 'named',
         globals: {
           vue: 'Vue',
+        },
+        assetFileNames: (chunkInfo) => {
+          if (chunkInfo.name === 'style.css')
+            return 'index.css'
+          return chunkInfo.name as string
         },
       },
     },
