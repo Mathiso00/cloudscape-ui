48f212ef9cbf369ffbe7c7bf31580da7f28285b1
Pacôme <dhiser.pacome@gmail.com>
2024-09-27 11:32:56 +0200

    feat: add cardplay component

--- src/components/application/card-play/CardPlay.vue
+++ src/components/application/card-play/CardPlay.vue
@@ -1,17 +1,27 @@
 <script setup lang="ts">
+const props = defineProps<{
+  description: string
+  title: string
+}>()
+
+function handleClick() {
+  // logique du bouton
+}
 </script>
 
 <template>
-  <div class="relative z-50 max-w-lg transform border border-white/10 bg-primary p-6 shadow-lg sm:rounded-lg">
-    <div class="flex items-center justify-between space-x-2xl">
-      <div class="h-32 w-2 rounded-sm bg-white" />
+  <div class="relative max-w-lg border border-white/10 bg-primary p-4 shadow-lg sm:rounded-lg">
+    <div class="flex items-center justify-between space-x-4">
+      <div class="h-30 w-1 flex-grow bg-white -mb-4 -mt-4" />
       <h5 class="text-2xl text-white font-bold">
-        lorem ipsum
+        {{ title }}
       </h5>
-      <p class="text-white font-normal">
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do dolore magna aliqua.
-      </p>
-      <button class="i-ph:play-circle-bold h-32 w-30 text-white" @click="handleClick" />
+      <div class="flex-column">
+        <p class="break-all text-white font-normal">
+          {{ description }}
+        </p>
+      </div>
+      <button class="i-ph:play-circle-bold ml-4 h-15 w-15 flex-shrink-0 text-white" @click="handleClick" />
     </div>
   </div>
 </template>
--- src/components/application/card-play/cardplay.stories.ts
+++ src/components/application/card-play/cardplay.stories.ts
@@ -1,17 +1,23 @@
-import type { ProgressBar } from '@/components'
 import type { Meta, StoryObj } from '@storybook/vue3'
 import { CardPlay } from '@/components'
 
 const meta = {
+  parameters: {
+    controls: {
+      excludes: ['text'],
+    },
+  },
   title: 'Application/CardPlay/Card Play',
   component: CardPlay,
   tags: ['autodocs'],
-} satisfies Meta<typeof ProgressBar>
+} satisfies Meta<typeof CardPlay>
 
 export default meta
-  type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
+    title: 'Card Play',
+    description: 'lorem ipsum lorem ipsum',
   },
 }
