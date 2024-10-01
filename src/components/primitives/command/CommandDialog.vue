352e91be5b3e6597aac45d29173fbc6b56790470
npldevfr <asnpldev@gmail.com>
2024-10-01 13:34:17 +0200

    style: Improve dialog and skeleton component styling

--- src/components/primitives/command/CommandDialog.vue
+++ src/components/primitives/command/CommandDialog.vue
@@ -1,18 +1,32 @@
 <script setup lang="ts">
 import type { DialogRootEmits, DialogRootProps } from 'radix-vue'
-import { Dialog, DialogContent } from '@/components'
-import { useForwardPropsEmits } from 'radix-vue'
+import { Dialog, DialogContent, DialogDescription, DialogTitle } from '@/components'
+import { useForwardPropsEmits, VisuallyHidden } from 'radix-vue'
 import Command from './Command.vue'
 
-const props = defineProps<DialogRootProps>()
+const props = defineProps<DialogRootProps & { isPressed?: boolean }>()
 const emits = defineEmits<DialogRootEmits>()
 
 const forwarded = useForwardPropsEmits(props, emits)
 </script>
 
 <template>
-  <Dialog v-bind="forwarded">
-    <DialogContent class="overflow-hidden p-0 shadow-lg !bg-primary-950">
+  <Dialog
+    :open="forwarded.open"
+    :default-open="forwarded.defaultOpen"
+    :modal="forwarded.modal"
+    @update:open="(e) => $emit('update:open', e)"
+  >
+    <DialogContent
+      class="overflow-hidden p-0 shadow-lg transition-transform !bg-primary-950"
+      :class="{
+        'scale-98': props.isPressed,
+      }"
+    >
+      <VisuallyHidden>
+        <DialogTitle>Command</DialogTitle>
+        <DialogDescription>Press up and down keys to navigate, press enter to execute</DialogDescription>
+      </VisuallyHidden>
       <Command class="[&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group]]:px-2 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group-heading]]:text-white [&_[cmdk-group-heading]]:font-medium">
         <slot />
       </Command>
--- src/components/primitives/skeleton/Skeleton.vue
+++ src/components/primitives/skeleton/Skeleton.vue
@@ -17,7 +17,7 @@ const { width, height } = defineProps<KoopsSkeletonProps>()
     class="relative select-none overflow-hidden rounded-md bg-primary-950 text-transparent"
   >
     <div class="absolute inset-0 bg-secondary-950" />
-    <div class="absolute inset-0 top-0 z-30 block h-full transform animate-shimmer animate-duration-1500 from-transparent to-secondary-900/10 bg-gradient-to-r -translate-x-full -skew-x-3" />
+    <div class="absolute inset-0 top-0 z-30 block h-full transform animate-shimmer animate-duration-1500 from-transparent to-secondary-900/10 bg-gradient-to-r -translate-x-120% -skew-x-10" />
     <slot />
   </div>
 </template>
