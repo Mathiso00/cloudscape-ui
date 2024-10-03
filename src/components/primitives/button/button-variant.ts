944d89b5889bbaf3d14a6680eeec5c7bdf30bff2
npldevfr <asnpldev@gmail.com>
2024-10-03 22:53:23 +0200

    style: Update button and tooltip styles for consistency

--- src/components/primitives/button/button-variant.ts
+++ src/components/primitives/button/button-variant.ts
@@ -5,7 +5,7 @@ export const buttonVariant = tv({
   base: 'inline-flex text-center shadow select-none font-500 gap-2.5 border border-transparent disabled:cursor-not-allowed items-center outline-transparent justify-center whitespace-nowrap transition-all ring-offset-2 ring-offset-inherit ring-transparent focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50',
   variants: {
     variant: {
-      'default': 'bg-white !text-secondary focus-visible:ring-white/10 focus-visible:ring-offset-transparent',
+      'default': 'bg-#DE6449 text-white focus-visible:ring-[#DE6449]/10 focus-visible:ring-offset-transparent',
       'outline': 'border-white/10 text-white bg-transparent hover:bg-white/5 focus-visible:ring-white/5 focus-visible:ring-offset-transparent',
       'destructive': 'bg-red-800 text-white hover:bg-red-600 focus-visible:ring-red-600/20 focus-visible:ring-offset-transparent',
       'secondary': 'bg-secondary-950 text-white hover:bg-secondary-950/90 focus-visible:ring-secondary/20 focus-visible:ring-offset-transparent',
--- src/components/primitives/command/CommandShortcut.vue
+++ src/components/primitives/command/CommandShortcut.vue
@@ -9,7 +9,7 @@ const forwarded = useForwardPropsEmits(props, emits)
 </script>
 
 <template>
-  <div class="flex select-none items-center gap-2 rounded-md p-1 transition-all hover:op-80">
+  <div class="flex select-none items-center gap-2 rounded-md transition-all hover:op-80">
     <Kbd no-margin v-bind="forwarded" class="min-h-5 min-w-5 flex items-center justify-center px-1 text-xl text-white/40" filled />
     <span class="mr-1 text-xs text-white opacity-50">{{ props.name }}</span>
   </div>
